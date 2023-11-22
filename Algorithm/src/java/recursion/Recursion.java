f53ee223f2f11f3af6d1614dd073afc4d3914b11
feng <feng>
2023-11-23 00:07:12 +0800

    递归：反向打印字符串

--- Algorithm/src/java/recursion/Recursion.java
+++ Algorithm/src/java/recursion/Recursion.java
@@ -2,8 +2,10 @@ package recursion;
 
 public class Recursion {
     public static void main(String[] args) {
-        int factorial = factorial(5);
-        System.out.println(factorial);
+//        int factorial = factorial(5);
+//        System.out.println(factorial);
+        String str = "abc";
+        reverseString(str,0);
     }
 
     //阶乘
@@ -12,6 +14,15 @@ public class Recursion {
             return 1;
         }
         return n*factorial(n-1);
+    }
+
+    //反向打印字符串       判断n是否等于长度，不等于就继续走递归，直到相等后就执行回来的逻辑，此时会一次次打印从后往前
+    public static void reverseString(String str,int n){
+        if (n==str.length()){
+            return;
+        }
+        reverseString(str,n+1);
+        System.out.println(str.charAt(n));
     }
 
 }
