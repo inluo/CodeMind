d0b37cb5d818b0e8cb34e581a8a5110504f8dd9c
Feng <1342550898@qq.com>
2024-06-29 18:02:23 +0800

    commit:设计模式

--- Design/src/test/proxy/DemoTest.java
+++ Design/src/test/proxy/DemoTest.java
@@ -1,6 +1,8 @@
 package proxy;
 
 import org.junit.jupiter.api.Test;
+import proxy.jdk_proxy.ProxyFactory;
+import proxy.jdk_proxy.SellTickets;
 import proxy.static_proxy.ProxyPoint;
 
 public class DemoTest {
@@ -8,5 +10,16 @@ public class DemoTest {
     public void test(){
         ProxyPoint pp = new ProxyPoint();
         pp.sell();
+    }
+
+    @Test
+    public void test1() {
+        //获取代理对象
+        ProxyFactory factory = new ProxyFactory();
+
+        SellTickets proxyObject = factory.getProxyObject();
+        proxyObject.sell();
+        System.out.println(proxyObject.getClass());
+
     }
 }
