4708658245e770fdee078523c08f686e2be70265
Feng <1342550898@qq.com>
2024-07-20 07:35:38 +0800

    commit:设计模式：模板方法模式

---
+++ Design/src/java/template/ConcreteClass_BaoCai.java
@@ -0,0 +1,15 @@
+package template;
+
+//实现细节不同的方法
+public class ConcreteClass_BaoCai extends AbstractClass{
+
+    @Override
+    public void pourVegetable() {
+        System.out.println("下锅的蔬菜是包菜");
+    }
+
+    @Override
+    public void pourSauce() {
+        System.out.println("下锅的酱料是辣椒");
+    }
+}
---
+++ Design/src/java/template/ConcreteClass_CaiXin.java
@@ -0,0 +1,14 @@
+package template;
+
+//实现细节不同的方法
+public class ConcreteClass_CaiXin extends AbstractClass {
+    @Override
+    public void pourVegetable() {
+        System.out.println("下锅的蔬菜是菜心");
+    }
+
+    @Override
+    public void pourSauce() {
+        System.out.println("下锅的酱料是蒜蓉");
+    }
+}
